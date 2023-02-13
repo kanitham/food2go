81fd63059114e85d246629bb83689d29669b9c34
Raja <krajgo123@hotmail.com>
2023-02-13 13:33:27 +0530

    Project basicsetup

---
+++ src/main/java/com/food2go/order2go/Order2goApplication.java
@@ -0,0 +1,13 @@
+package com.food2go.order2go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order2goApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Order2goApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food2go/order2go/Order2goApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food2go.order2go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Order2goApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
